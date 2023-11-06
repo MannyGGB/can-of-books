c91fba47de82ed9e974a2391ddda331e72c852ad
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-06 14:59:23 +0000

    Fix deployment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
+const PORT = 8080;
+
 const mongoose = require("mongoose");
-//mongoose.connect(process.env.DATABASE);
+//const Book = require("./models/book");
+//mongoose.connect(process.env.DATABASE_URL);
 
 // add your endpoints here
 app.get("/", (request, response) => response.json("Root route for translatim"));

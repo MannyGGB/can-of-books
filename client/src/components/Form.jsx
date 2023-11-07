47cbb071a6daa8a565c0ff1e85a5a794f2b1ad25
Stephen <sagelvl40@gmail.com>
2023-11-07 22:13:35 +0000

    put form and home back to how they were before stephen tried the modal

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function Form({ onSubmit }) {
+export default function Form({ books, setBooks }) {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,27 +1,9 @@
-import { useState } from "react";
-import BookFormModal from "../components/BookFormModal";
 import Form from "../components/Form";
 
 export default function Home({ books, setBooks, deleteBook }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleFormSubmit = (newBookData) => {
-    setBooks([...books, newBookData]);
-    closeModal();
-  };
-
   return (
     <div>
       <h2>Home Page</h2>
-      <button onClick={openModal}>Add Book</button>
       <Form books={books} setBooks={setBooks} />
       {books.length > 0 &&
         books.map((book) => (
@@ -39,11 +21,6 @@ export default function Home({ books, setBooks, deleteBook }) {
           </article>
         ))}
       {books.length < 0 && <p>The book collection is empty</p>}
-      <BookFormModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        onSubmit={handleFormSubmit}
-      />
     </div>
   );
 }

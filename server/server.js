c90ecf4d0ae430e5f7e13e09fea92bff53ad51c5
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-07 15:37:57 +0000

    Deploy debug2

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 const PORT = 8080;
 
-// const mongoose = require("mongoose");
-// const Book = require("./models/book");
-// mongoose.connect(process.env.DATABASE_URL);
+const mongoose = require("mongoose");
+const Book = require("./models/book");
+mongoose.connect(process.env.DATABASE_URL);
 
 // add your endpoints here
 app.get("/", (request, response) => response.json("Root route for books."));

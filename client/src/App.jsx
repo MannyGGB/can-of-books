04a73db9fccfd1e76efe21edc10df8f34d12f5a9
Stephen <sagelvl40@gmail.com>
2023-11-09 22:08:09 +0000

    try and add exclusive database amendments based on who is logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,37 @@ import axios from "axios";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Book from "./pages/Book";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 const API_Url = import.meta.env.VITE_SERVER_URL || "http://localhost:8080";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     getBooks();
-  }, []);
+  }, [isAuthenticated]);
 
   async function getBooks() {
     const API = `${API_Url}/books`;
-    const res = await axios.get(API);
-    setBooks(res.data);
+
+    const userId = isAuthenticated ? user.sub : null;
+
+    try {
+      const res = await axios.get(API, {
+        params: { userId },
+      });
+
+      setBooks(res.data);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
   }
+
   async function deleteBook(id) {
     const check = confirm("Are you sure?");
     if (check) {
@@ -30,6 +46,7 @@ function App() {
       alert("Good choice!");
     }
   }
+
   return (
     <BrowserRouter>
       <>
@@ -37,6 +54,15 @@ function App() {
           <h1>Can of Books</h1>
           <p>Your trusted book collector</p>
         </header>
+        {isAuthenticated ? (
+          <div>
+            <Profile />
+            <LogoutButton />
+          </div>
+        ) : (
+          <LoginButton />
+        )}
+
         <nav>
           <ul>
             <li>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-ouuggwo86fqcirbe.uk.auth0.com"
+      clientId="4TGWn44zGDbR2kCS9OyXCP3b0KpBR58q"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+    ,
   </React.StrictMode>
 );
